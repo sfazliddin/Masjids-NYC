cc075a598b1b5e9a817ea46e75198f83147ec6b3
Samariddin <gotenks.ssj499@gmail.com>
2023-02-12 19:50:58 -0500

    first commit for Masjid Locator

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+ 
+
+  return (
+    <div className="App">
+      <button>See Masjids in NYC</button>
+      
+    </div>
+  )
+}
+
+export default App
